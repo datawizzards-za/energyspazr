cb7ebbc262ce82d253f5fbfd1b880cbd5488cb92
Lebogo Ofentswe <ofentswel@gmail.com>
2017-06-09 22:55:52 +0200

    Authentication settings.py

---
+++ energyspazr/auth.py
@@ -0,0 +1,8 @@
+class PostgreSQLAuth:
+    app_db = {
+        'NAME': 'energyspazr',
+        'USER': 'energy',
+        'PASS': 'x9308PslYT@#^&',
+        'PORT': '5432',
+        'HOST': 'localhost',
+    }
--- energyspazr/settings.py
+++ energyspazr/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+from energyspazr.auth import PostgreSQLAuth
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'energyspazr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': PostgreSQLAuth.app_db['NAME'],
+        'HOST': PostgreSQLAuth.app_db['HOST'],
+        'USER': PostgreSQLAuth.app_db['USER'],
+        'PORT': PostgreSQLAuth.app_db['PORT'],
+        'PASSWORD': PostgreSQLAuth.app_db['PASS'],
     }
 }
 
