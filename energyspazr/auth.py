d2300cc0a263790e235e88b45ab4fd3ed235ee45
Lebogo Ofentswe <ofentswel@gmail.com>
2017-06-09 23:14:50 +0200

    Authentication auth.py changed

--- energyspazr/auth.py
+++ energyspazr/auth.py
@@ -2,7 +2,7 @@ class PostgreSQLAuth:
     app_db = {
         'NAME': 'energyspazr',
         'USER': 'energy',
-        'PASS': 'x9308PslYT@#^&',
+        'PASS': 'x9308PslyT@#^&',
         'PORT': '5432',
         'HOST': 'localhost',
     }
