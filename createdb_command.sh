17dff7f374d92187afb96d8e8322442f1d9e5287
Mabu <manailengmj@gmail.com>
2017-06-10 11:55:06 +0200

    Fix: createdb script for Mac

--- createdb_command.sh
+++ createdb_command.sh
@@ -1,6 +1,11 @@
-sudo -i -u postgres psql -c "CREATE USER energy WITH PASSWORD 'x9308PslyT@#^&';"
-sudo -i -u postgres psql -c "CREATE DATABASE energyspazr OWNER energy;"
-sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE energyspazr to energy;"
-# for mac 
+#for Mac
+psql postgres -c "CREATE USER energy WITH PASSWORD 'x9308PslyT@#^&';"
+psql postgres -c "CREATE DATABASE energyspazr OWNER energy;"
+psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE energyspazr to energy;"
+
+#Ubuntu
+#sudo -i -u postgres psql -c "CREATE USER energy WITH PASSWORD 'x9308PslyT@#^&';"
+#sudo -i -u postgres psql -c "CREATE DATABASE energyspazr OWNER energy;"
+#sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE energyspazr to energy;"
 
 
