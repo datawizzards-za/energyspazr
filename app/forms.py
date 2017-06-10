89359b2fd8b85413952cc70571710aec6dcd45e4
Mabu <manailengmj@gmail.com>
2017-06-10 14:53:47 +0200

    UI: signin and signup forms

--- app/forms.py
+++ app/forms.py
@@ -6,12 +6,11 @@ from crispy_forms.layout import Submit, HTML, Div, Field
 from crispy_forms.bootstrap import FormActions
 from django import forms
 
-class LoginForm(AuthenticationForm):
-    helper = FormHelper()
-    helper.form_tag = False
-
+class SigninForm(AuthenticationForm):
+    
     class Meta(AuthenticationForm):
         model = User
+        fields = ['username', 'password']
 
     helper = FormHelper()
     helper.form_method = 'POST'
@@ -21,22 +20,18 @@ class LoginForm(AuthenticationForm):
         HTML('<h3 class="login-head"><i class="fa fa-lg fa-fw fa-user">'\
             '</i>SIGN IN</h3>'),
         Div(
-            HTML('<label class="control-label">USERNAME</label>'),
-            Field('username', css_class='form-control', autofocus=True),
+            Field('username', css_class='form-control text-center', placeholder='Email Address', autofocus=True),
             css_class='form-group'
         ),
         Div(
-            HTML('<label class="control-label">PASSWORD</label>'),
-             Field('password', css_class='form-control'),
+             Field('password', placeholder='Password', css_class='form-control text-center'),
             css_class='form-group'
         ),
         Div(
             Div(
                 Div(
                     HTML('<label class="semibold-text">'\
-                        '<input type="checkbox">'\
-                        '<span class="label-text">'\
-                        'Stay Signed in</span></label>'),
+                        '<input type="checkbox"></label>'),
                     css_class='animated-checkbox'
                 ),
                 HTML('<p class="semibold-text mb-0">'\
@@ -49,20 +44,25 @@ class LoginForm(AuthenticationForm):
             FormActions(Submit('login', 'SIGN IN',
                                    css_class='btn btn-primary btn-block')),
             css_class='form-group btn-container'
+        ),
+        Div(
+            Div(
+                HTML('<br /><p class="semibold-text mb-0">' \
+                     "<a href='{% url 'signup' %}'>Already Registered?</a></p>")
+            ),
+            css_class='form-group text-center'
         )
     )
 
 
-class RegistrationForm(UserCreationForm):
-    password1 = forms.PasswordInput()
+class SignupForm(UserCreationForm):
+    
     class Meta:
         model = User
-        fields = ['username', 'password','email',
-                  'first_name', 'last_name']
+        fields = ['first_name', 'last_name', 'email']
 
     helper = FormHelper()
     helper.form_method = 'POST'
-    helper.form_class = 'login-form'
     helper.form_show_labels = False
 
     helper.layout = Layout(
@@ -70,38 +70,49 @@ class RegistrationForm(UserCreationForm):
              '</i>SIGN UP</h3>'),
         Div(
             Div(
-                Field('first_name', css_class='form-control', placeholder='First Name', autofocus=True),
-                css_class='col-md-4'
-            ),
-            Div(
-                Field('last_name', css_class='form-control', placeholder='Last Name'),
-                css_class='col-md-4'
+                Field('first_name', css_class='form-control text-center', placeholder='First Name', autofocus=True),
+                css_class='col-md-12'
             ),
             css_class='row mb-20'
         ),
         Div(
-            HTML('<label class="control-label">PASSWORD</label>'),
-            Field('password', css_class='form-control'),
-            css_class='form-group'
+            Div(
+                Field('last_name', css_class='form-control text-center', placeholder='Last Name'),
+                css_class='col-md-12'
+            ),
+            css_class='row mb-20'
         ),
         Div(
             Div(
-                Div(
-                    HTML('<label class="semibold-text">' \
-                         '<input type="checkbox">' \
-                         '<span class="label-text">' \
-                         'Stay Signed in</span></label>'),
-                    css_class='animated-checkbox'
-                ),
-                HTML('<p class="semibold-text mb-0">' \
-                     '<a data-toggle="flip">Forgot Password ?</a></p>'),
-                css_class='utility'
+                Field('email', css_class='form-control text-center', placeholder='Email Address'),
+                css_class='col-md-12'
             ),
-            css_class='form-group'
+            css_class='row mb-20'
         ),
         Div(
-            FormActions(Submit('login', 'SIGN IN',
+            Div(
+                Field('password1', css_class='form-control text-center ', placeholder='Password'),
+                css_class='col-md-12'
+            ),
+            css_class='row mb-20'
+        ),
+        Div(
+            Div(
+                Field('password2', css_class='form-control text-center ', placeholder='Confirm Password'),
+                css_class='col-md-12'
+            ),
+            css_class='row mb-20'
+        ),
+        Div(
+            FormActions(Submit('login', 'SIGN UP',
                                css_class='btn btn-primary btn-block')),
             css_class='form-group btn-container'
+        ),
+        Div(
+            Div(
+                HTML('<br /><p class="semibold-text mb-0 text-center">' \
+                     "<a href='{% url 'signin' %}'>Already Registered?</a></p>")
+            ),
+            css_class='form-group'
         )
     )
--- energyspazr/settings.py
+++ energyspazr/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.sites',
     'django.contrib.auth',
-    'registration',
+#    'registration',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
