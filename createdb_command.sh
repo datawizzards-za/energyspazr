ad1d3aecc79259430b965237d7a76f32614a5065
Mabu <manailengmj@gmail.com>
2017-06-10 11:31:56 +0200

    UI: debuging postgres

--- createdb_command.sh
+++
@@ -1,4 +0,0 @@
-sudo -i -u postgres psql -c "CREATE USER energy WITH PASSWORD 'x9308PslyT@#^&';"
-sudo -i -u postgres psql -c "CREATE DATABASE energyspazr OWNER energy;"
-sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE energyspazr to energy;"
-
