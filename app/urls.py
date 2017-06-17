67cf9045e3956575558370493c0325c081cfeeaa
Asive Dlaba <adlaba@csir.co.za>
2017-06-17 21:44:24 +0200

    fix(app.views, app.urls): restore 'supplier_installer' url

--- app/urls.py
+++ app/urls.py
@@ -14,39 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-
-from app.views import Dashboard, Home, FinancierUpdateAccount, OurProducts, PVT, \
-    SolarComponent, SolarGeyser, Register, ClientOrder, UserRoleView, OrderGeyser, \
-    OrderPVTSystem
-
-
-#from registration.backends.default.views import RegistrationView
-
-#from app.views import Registration
+from app import views
 
 
 urlpatterns = [
-    url(r'^\Z', Home.as_view(), name='home'),
-    #url(r'^\Z', Dashboard.as_view(), name='dashboard'),
-    url(r'^app/$', Home.as_view(), name='dashboard'),
-    url(r'^dashboard/$', Dashboard.as_view(), name='dashboard'),
-    url(r'^financier/$', FinancierUpdateAccount.as_view(), name='financier'),
-    url(r'^products/', OurProducts.as_view(), name='our-products'),
-    url(r'^products/pvt/', PVT.as_view(), name='pvt'),
-    url(r'^pvt-order/', OrderPVTSystem.as_view(), name='pvt-order'),
-    url(r'^geyser-order/', OrderGeyser.as_view(), name='geyser-order'),
-    url(r'^products/geyser/', SolarGeyser.as_view(), name='geyser'),
-    url(r'^products/component/', SolarComponent.as_view(), name='component'),
-    url(r'^register/', Register.as_view(), name='register'),
-    url(r'^order/', ClientOrder.as_view(), name='order'),
-    url(r'^user_roles/$', UserRoleView.as_view(), name='user_roles'),
-    url(r'^products/', OurProducts.as_view(), name='our_products'),
-    url(r'^products/pvt/', PVT.as_view(), name='pvt'),
-    url(r'^products/geyser/', SolarGeyser.as_view(), name='geyser'),
-    url(r'^products/component/', SolarComponent.as_view(), name='component'),
-    url(r'^register/', Register.as_view(), name='register'),
-
-    #url(r'^register/$', Registration.as_view()),
-
-    #url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^\Z', views.Home.as_view(), name='home'),
+    url(r'^app/$', views.Home.as_view(), name='dashboard'),
+    url(r'^dashboard/$', views.Dashboard.as_view(), name='dashboard'),
+    url(r'^financier/$', views.FinancierUpdateAccount.as_view(), name='financier'),
+    url(r'^supplier_installer/$', views.SupplierInstallerUpdateAccount.as_view(), 
+        name='supplier_installer'),
+    url(r'^products/$', views.OurProducts.as_view(), name='our-products'),
+    url(r'^products/pvt/$', views.PVT.as_view(), name='pvt'),
+    url(r'^pvt-order/$', views.OrderPVTSystem.as_view(), name='pvt-order'),
+    url(r'^geyser-order/$', views.OrderGeyser.as_view(), name='geyser-order'),
+    url(r'^products/geyser/$', views.SolarGeyser.as_view(), name='geyser'),
+    url(r'^products/component/$', views.SolarComponent.as_view(), name='component'),
+    url(r'^register/$', views.Register.as_view(), name='register'),
+    url(r'^order/$', views.ClientOrder.as_view(), name='order'),
+    url(r'^user_roles/$', views.UserRoleView.as_view(), name='user_roles'),
+    url(r'^products/$', views.OurProducts.as_view(), name='our_products'),
+    url(r'^products/pvt/$', views.PVT.as_view(), name='pvt'),
+    url(r'^products/geyser/$', views.SolarGeyser.as_view(), name='geyser'),
+    url(r'^products/component/$', views.SolarComponent.as_view(), name='component'),
+    url(r'^register/$', views.Register.as_view(), name='register'),
 ]
