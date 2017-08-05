2d294fece2df0b08714e131a3d790bf2def386bb
Mabu <manailengmj@gmail.com>
2017-08-05 23:41:55 +0200

    script

---
+++ migrations.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+# delete migrations
+echo "Deleting migrations..."
+rm -v app/migrations/000*
+
+echo ""
+echo "Making migrations..."
+# make migatrions then migrate
+python manage.py makemigrations
+python manage.py migrate
+
+echo ""
+echo "Loading fixtures..."
+# load fixtuxes in order
+python manage.py loaddata app/fixtures/initial_brands.yaml
+python manage.py loaddata app/fixtures/initial_products.yaml
+python manage.py loaddata app/fixtures/initial_batteries.yaml
+python manage.py loaddata app/fixtures/initial_panels.yaml
+python manage.py loaddata app/fixtures/initial_dimension_names.yaml
+python manage.py loaddata app/fixtures/initial_appliances.yaml
+python manage.py loaddata app/fixtures/*.yaml
