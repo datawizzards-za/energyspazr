2ece232e5fd4a41776c0d5b5d94ab7beb7b27327
Mabu <manailengmj@gmail.com>
2017-08-13 16:48:41 +0200

    Script

--- migrations.sh
+++ migrations.sh
@@ -2,13 +2,18 @@
 
 MIGS=$1
 
+#Comment OPTIONS in forms
+echo "Commenting OPTIONS in froms.py"
+sed -i '' 's/OPTIONS = ((p.name, p.name) for p in models.Appliance.objects.all())/#OPTIONS = ((p.name, p.name) for p in models.Appliance.objects.all())/' app/forms.py
+sed -i '' 's/name = forms.ChoiceField(choices=OPTIONS, required=True)/#name = forms.ChoiceField(choices=OPTIONS, required=True)/' app/forms.py
+
 # delete migrations
 echo "Deleting migrations..."
 rm -v app/migrations/000*
 
 echo ""
 echo "Making migrations..."
-make migatrions then migrate
+echo "make migatrions then migrate"
 python manage.py makemigrations
 python manage.py migrate
 
@@ -20,4 +25,10 @@ for mig in `cat $MIGS`; do
     python manage.py loaddata $mig;
     done
 
+#UnComment OPTIONS in forms
+echo "Uncommenting OPTIONS in froms.py"
+sed -i '' 's/#OPTIONS = ((p.name, p.name) for p in models.Appliance.objects.all())/OPTIONS = ((p.name, p.name) for p in models.Appliance.objects.all())/' app/forms.py
+sed -i '' 's/#name = forms.ChoiceField(choices=OPTIONS, required=True)/name = forms.ChoiceField(choices=OPTIONS, required=True)/' app/forms.py
+
+echo ""
 echo "DONE!"
